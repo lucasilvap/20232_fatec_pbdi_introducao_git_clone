11e719a95612cbe782ff2e4cdb0e256f2b63599b
lucasilvap <silva.pereira.92@gmail.com>
2023-09-02 15:59:53 -0300

    ap01(git): funcao menu implementada

--- menu.py
+++ menu.py
@@ -7,4 +7,24 @@ def menu():
     print('4 - Dividir')
     print('0 - Sair')
     opcao = int(input('Digite a opção desejada: '))
+    return opcao
+
+def main():
+    while True:
+        opcao = menu()
+        if opcao == 1:
+            print(calculadora.somar(2, 2))
+        elif opcao == 2:
+            print(calculadora.subtrair(2, 2))
+        elif opcao == 3:
+            print(calculadora.multiplicar(2, 2))
+        elif opcao == 4:
+            print(calculadora.dividir(2, 2))
+        elif opcao == 0:
+            break
+        else:
+            print('Opção inválida!')
+
+main()
+
     
